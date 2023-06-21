3e7724603def53d6253bef6f07263a80faf314b9
Caio Peluti <cpeluti@gmail.com>
2023-06-20 22:45:21 -0300

    componentes essenciais adicionados ao frontend

---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    import type { Message } from '@/interfaces/message';
+    defineProps<{
+        messages: Message[]
+    }>()
+
+</script>
+<template>
+    <h1> teste </h1>
+    <div v-for="m in messages">{{m.content}}</div>
+</template>
---
+++ frontend/src/components/Textbox.vue
@@ -0,0 +1,13 @@
+<template>
+    <textarea v-model="text"></textarea>
+    <button @click="emit('sendMessage', text)">enviar</button>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const emit = defineEmits<{
+        sendMessage: [text: string]
+    }>()
+    const text = ref("")
+</script>
