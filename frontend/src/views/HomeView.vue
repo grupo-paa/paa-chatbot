66e222b975031ea1501f9928e4d1aa087602b3f3
Caio Peluti <cpeluti@gmail.com>
2023-06-20 22:45:21 -0300

    componentes essenciais adicionados ao frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,28 +1,35 @@
 <template>
   <main>
-    <TheWelcome />
-    <h1>Aqui  </h1>
-    <h1>{{ message }}</h1>
+    <Messages :messages="messages"/>
+    <Textbox @send-message="(text)=>{sendMessage(text)}"/>
   </main>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue';
+
+  import Messages from '../components/Messages.vue'
+  import Textbox from '../components/Textbox.vue'
+  import type {Message} from '../interfaces/message'
+  import { onMounted, ref } from 'vue';
+  import type {Ref} from 'vue'
   import axios from 'axios';
 
-  const message = ref('');
-  console.log("aqui")
-  axios.get('http://localhost:5000/api/data')
-    .then(response => {
-      message.value = response.data.message;
-    })
-    .catch(error => {
-      console.error(error);
-    });
-
-  const setup = () => {
-    return {
-      message
-    };
-  };
+  const messages: Ref<Message[]> = ref([])
+  const sendMessage = async (text: string)=>{
+    messages.value = [...messages.value, {sender: 'user', content: text}] 
+    try {
+      let res = await axios({
+          method:"post",
+          url: "http://localhost:5000/message",
+          data: {message:text}
+      });
+      let receivedMessage: Message = res.data
+      messages.value = [...messages.value, receivedMessage] 
+    } catch (e){
+      axios.isAxiosError(e) ? console.log('axios:', e) : console.log(e);
+    }
+  }
+  onMounted(async () => {
+    await sendMessage("teste2")
+  })
 </script>
