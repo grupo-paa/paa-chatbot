7fb2b26c196569ef2b82765c9352a168a1f8d2c2
Caio Peluti <cpeluti@gmail.com>
2023-06-25 14:00:10 -0300

    tailwind

--- frontend/src/components/Messages.vue
+++ frontend/src/components/Messages.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
     import type { Message } from '@/interfaces/message';
+import { ref } from 'vue';
     defineProps<{
         messages: Message[]
     }>()
-
+    const test = ref(['coisa 1', 'coisa 2'])
+    console.log(test.value)
 </script>
 <template>
     <h1> teste </h1>
     <div v-for="m in messages">{{m.content}}</div>
 </template>
+<style scoped>
+h2{
+    background-color: red;
+}
+</style>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -10,7 +10,7 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/aboutteste',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
