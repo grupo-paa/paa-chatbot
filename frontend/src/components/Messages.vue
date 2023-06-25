947fb268d267921c3d9ee56c1d7e3f39e674645b
AnaJordao <acjpjvjp@gmail.com>
2023-06-25 11:58:00 -0300

    Componente de input do chat feito

--- frontend/src/components/Messages.vue
+++ frontend/src/components/Messages.vue
@@ -9,3 +9,11 @@
     <h1> teste </h1>
     <div v-for="m in messages">{{m.content}}</div>
 </template>
+
+<style scoped>
+    
+
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
