47e26b97d6c881e136e00e3fcc61069fe62e7e3e
Caio Peluti <cpeluti@gmail.com>
2023-06-25 16:58:33 -0300

    tela de login

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="">
-    <h1>login</h1>
-  </div>
+    <div class="row window-height window-width justify-center items-center">
+        <q-form class="q-gutter-md" @submit="onSubmit" @reset="onReset">
+            <q-input 
+                v-model="user" 
+                placeholder="Usuario"
+                lazy-rules
+                :rules="[val => val && val.length > 0 || 'Por favor digite um usuário']"
+            />
+            <q-input 
+                v-model="password" 
+                placeholder="Senha"
+                lazy-rules
+                :rules="[val => val && val.length > 0 || 'Por favor digite uma senha']"
+            />
+            <q-btn type="submit" label="logar" color="secondary"/>
+        </q-form>
+    </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue'
+const user = ref()
+const password = ref()
+const onSubmit = () => {
+    console.log(user.value, password.value)
+}
+const onReset = () => {
+    user.value = null
+    password.value = null
+}
+
 </script>
 
 <style scoped>
