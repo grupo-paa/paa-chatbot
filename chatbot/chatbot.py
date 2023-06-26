74de5b1c1c6e24380b586c3d9df91f46c8eecdff
bragars <bragareispedro@gmail.com>
2023-06-25 23:15:48 -0300

    Some corrections

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -7,7 +7,7 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-from tensorflow.keras.model import load_model
+from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
