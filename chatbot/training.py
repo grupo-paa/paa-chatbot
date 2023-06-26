68cc832119b6ed9e0f1334571588510688609340
bragars <bragareispedro@gmail.com>
2023-06-26 07:34:29 -0300

    Code running

--- chatbot/training.py
+++ chatbot/training.py
@@ -9,8 +9,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer()
-
 intents = json.loads(open('intents.json').read())
 
 words = []
@@ -19,17 +17,27 @@ documents = []
 ignore_letters = ['?', '!', '.', ',']
 
 for intent in intents['intents']:
-    for pattern in intent['patterns']:
-      word_list = nltk.word_tokenize(pattern)
-      words.append(word_list)
-      documents.append((word_list, intent['tag']))
-      if intent['tag'] not in classes:
-        classes.append(intent['tag'])
+  for pattern in intent['patterns']:
+    word_list = nltk.word_tokenize(pattern)
+    words.extend(word_list)
+    documents.append((word_list, intent['tag']))
 
-print(documents)
+    if intent['tag'] not in classes:
+      classes.append(intent['tag'])
 
+lemmatizer = WordNetLemmatizer()
+
+# words = []
+# for word in words:
+#   print(word)
+#   print("lemma", lemmatizer.lemmatize(word))
+#   if word not in ignore_letters:
+#     word.append(lemmatizer.lemmatize(word))
+# words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+# words = [lemmatizer.lemmatize(word) for word_list in words for word in word_list if word not in ignore_letters]
 words = sorted(set(words))
+print('words', words)
 
 classes = sorted(set(classes))
 
@@ -40,32 +48,32 @@ training = []
 output_empty = [0] * len(classes)
 
 for document in documents:
-  bag = []
-  word_patterns = document[0]
-  word_patterns = [lemmatizer.lemmatizer(word.lower()) for word in word_patterns]
-  for word in words:
-    bag.append(1) if word in word_patterns else bag.append(0)
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
 
-  output_row = list(output_empty)
-  output_row[classes.index(document[1])] = 1
-  training.append([bag, output_row])
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype=object)
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]), ), activation='relu'))
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.h5', hist)
+model.save('chatbot_model.h5')
 print('Done')
